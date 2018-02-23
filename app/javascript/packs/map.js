633dc5e751c097bdf5aacd9222cef40dcbaf7988
Lucas S <lucas.soaresouza@gmail.com>
2018-02-22 22:39:44 -0300

    Returning the old way of loading the buildings

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,3 +1,3 @@
-import Buildings from "../map/buildings"
-const buildings = new Buildings();
-buildings.addToMap();
+// import Buildings from "../map/buildings"
+// const buildings = new Buildings();
+// buildings.addToMap();
